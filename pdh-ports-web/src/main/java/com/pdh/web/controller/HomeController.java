f148a8a4a8667153cdd6e97fb178db5781ff19c2
Michael Valencia <valencia.mikeangelo@gmail.com>
2014-01-22 15:01:40 +0800

    Initial Login Page with Boostrap

--- pdh-ports-web/src/main/java/com/pdh/web/controller/HomeController.java
+++ pdh-ports-web/src/main/java/com/pdh/web/controller/HomeController.java
@@ -4,7 +4,10 @@
 package com.pdh.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pdh.web.model.PageContent;
 
 /**
  * @author Michael
@@ -15,7 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
 	@RequestMapping("/home")
-	public String home(){
+	public String home(ModelMap map){
+		
+		PageContent pageData= new PageContent();
+		pageData.setTitle("Welcome to Pinoy Developer Hub");
+		map.put("pageData",pageData);
 		return "home";
 	}
 }
---
+++ pdh-ports-web/src/main/java/com/pdh/web/model/PageContent.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.pdh.web.model;
+
+/**
+ * @author Michael
+ * Jan 22, 2014
+ */
+public class PageContent {
+	
+	private String title;
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+}
